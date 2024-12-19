165563cf2e31f843ef1e7e03708c19151c998588
giabao <dgbaoo2104@gmail.com>
2024-12-19 20:18:58 +0700

    xin chao

--- src/models/connect.js
+++ src/models/connect.js
@@ -2,13 +2,16 @@ import { Sequelize } from "sequelize";
 import configDb from "../config/connect_db.js";
 
 const sequelize = new Sequelize(
-  configDb.database, //tên database
-  configDb.user, // ten user
-  configDb.pass, //password user
+  // configDb.database, //tên database
+  // configDb.user, // ten user
+  // configDb.pass, //password user
+  "khoa_hoc", //tên database
+  "root", // ten user
+  "123456", //password user
   {
-    host: configDb.host,
-    port: configDb.port,
-    dialect: configDb.dialect,
+    host: "localhost",
+    port: 3308,
+    dialect: "mysql",
   }
 );
 
