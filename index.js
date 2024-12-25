9b8cae308c47f37045606ce556f9b5effb5ff9cf
giabao <dgbaoo2104@gmail.com>
2024-12-26 00:17:53 +0700

    xin chào

--- index.js
+++ index.js
@@ -6,30 +6,26 @@ import cors from "cors";
 import path from "path";
 
 // tạo đối tượng
-const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-};
+
 const app = express();
 app.use(express.json());
 
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static("public"));
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
-    origin: "https://ban-khoa-hoc-truc-tuyen.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+// app.use(
+//   cors({
+//     origin: "https://ban-khoa-hoc-truc-tuyen.vercel.app",
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("CHao dang gia bao ");
--- src/models/connect.js
+++ src/models/connect.js
@@ -5,6 +5,9 @@ const sequelize = new Sequelize(
   // configDb.database, //tên database
   // configDb.user, // ten user
   // configDb.pass, //password user
+  // "khoa_hoc", //tên database
+  // "root", // ten user
+  // "123456", //password user
   "khoa_hoc", //tên database
   "root", // ten user
   "123456", //password user
@@ -14,17 +17,5 @@ const sequelize = new Sequelize(
     dialect: "mysql",
   }
 );
-
-// Kiểm tra kết nối
-// const testConnection = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Kết nối đến cơ sở dữ liệu thành công.");
-//   } catch (error) {
-//     console.error("Không thể kết nối đến cơ sở dữ liệu:", error);
-//   }
-// };
-
-// testConnection();
 
 export default sequelize;
